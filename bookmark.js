d6807fee38811525b546f7b93e434b0e0657a1ae
Patrick Herzberg <patrick@Patricks-MacBook-Air.local>
2012-11-29 21:12:56 +0100

    Updated Version

--- bookmark.js
+++ bookmark.js
@@ -1,1 +1,2 @@
-javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://permalink.dev/permalink.js';})();
+/* use this as bookmark */
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://raw.github.com/westberliner/vbb_permalink/master/permalink.js';})();
--- permalink.js
+++ permalink.js
@@ -7,34 +7,66 @@
 vbb_permalink_init();
 
 function vbb_permalink_init() {
+	/* add head */
+	var tbody = document.getElementsByClassName('overviewTableHead');
+	if(vbb_permalink_has_class(tbody[0],'vbb-permalink')) {
+		return false;
+	}
+
+	tbody[0].className += " vbb-permalink";
+
+	var rows = tbody[0].getElementsByTagName("tr");
+	var cell = document.createElement("td");       
+		cell.innerHTML = "Permalink"; 
+		cell.style.padding = "2px 10px";      
+		rows[0].appendChild(cell);
+
+
 	/* apply share link */
-	var table = document.getElementsByClassName('resultTable');
-	table.getElementsByClassName('');
+	tbody = document.getElementById('conGroup_Foot');
+
+	var thead = tbody.getElementsByClassName('separatorSmall');
+	thead[0].colSpan += 1;
+
+	rows = tbody.getElementsByClassName('tpOverview');
+
+	for(i = 0; i < rows.length; i++) {
+		if(!vbb_permalink_has_class(rows[i],'navi')) {
+
+			var cell = document.createElement("td");        
+			cell.innerHTML = "<a href=\""+vbb_permalink_get_url(rows[i])+"\">share this</a>"; 
+			cell.style.padding = "2px 10px";        
+			rows[i].appendChild(cell);   
+		} else {
+			var navi = rows[i].getElementsByClassName('last');
+			navi[0].colSpan +=1;
+		} 
+	}
 }
 
 function vbb_permalink_get_url(row) {
 	var from = document.getElementById('HFS_from').value,
 		to = document.getElementById('HFS_to').value,
-		time = document.getElementById('HFS_time_REQ0').value,
 		date = document.getElementById('HFS_date_REQ0').value,
-		timesel = !!document.getElementById('HFS_timesel_REQ0_1').checked ? 'depart' : 'arrive',
-		hwai = [],
+		hwai = row.id.substring(10),
 		url = '',
 		details = document.getElementsByClassName('details');
+		
+		var timeDom = row.getElementsByClassName('planed');
+		time = timeDom[0].innerHTML.substring(1,6);
 
-	for(var i = 0; i < details.length; i++) {
-	  if (details[i].className.indexOf('menuLinks') !== -1 || details[i].className.indexOf('hide') !== -1) {
-	    continue;
-	  }
-	  hwai.push(details[i].id.substring(2));
-	}
-	url = 'dn?From=' + from + '%21&To=' + to + '%21&time=' + time + '&date=' + date + '&start=1&timesel=' + timesel;
+	url = 'dn?From=' + from + '%21&To=' + to + '%21&time=' + time + '&date=' + date + '&start=1' ;
+	url += '&HWAI=CONNECTION$' + hwai + '!id=' + hwai + '!HwaiConId=' + hwai + '!HwaiDetailStatus=details';
 
-	if(hwai.length) {
-		for(i = 0; i < hwai.length; i++) {
-			url += '&HWAI=CONNECTION$' + hwai[i] + '!id=' + hwai[i] + '!HwaiConId=' + hwai[i] + '!HwaiDetailStatus=details&seqnr=25';
-		}
-	}
 	return url;
-	//history.pushState({}, "", url);
 }
+
+function vbb_permalink_has_class(el, selector) {
+   var className = " " + selector + " ";
+   if ((" " + el.className + " ").replace(/[\n\t]/g, " ").indexOf(className) > -1) {
+    return true;
+   }
+   return false;
+ }
+
+
